d7518c83f2251b3fcafe8a2e211789ee6edebf0e
majac lim <majac6@ecubelabs.com>
2015-08-11 14:11:40 +0900

    no message

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-korean address finder - form daum
+보다 자세한 설명은 아래 링크를 참조해주세요.
+
+http://postcode.map.daum.net/guide
+
+한국 미티어 사용자분들을 위해 추가했습니다.
--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'majac:daum-postcode',
-  version: '0.0.1',
+  version: 'v2',
   // Brief, one-line summary of the package.
-  summary: 'http://http://postcode.map.daum.net/guide',
+  summary: 'korea address finder - daum APIs',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/majac6/package_daum-postcode.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
